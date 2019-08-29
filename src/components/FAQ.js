8641a0f6fa990fac2d4770cf30d94f0106ae34cd
daniel <daniel.lalasa@gmail.com>
2019-08-29 16:29:08 +0430

    FAQ question added

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -42,7 +42,7 @@ class FAQ extends React.PureComponent {
                 هدف اصلی ما در استارتاپ اسپیس ایجاد مرجع جامعی از فضاهای کاری
                 مشترک در سطح شهر می باشد تا استارتاپ ها و فری لنسرها هرچه راحتتر
                 و سریعتر فضای کاری مورد نیاز خود را یافته و کار مورد علاقه خود
-                را شروع کنند.
+                را شروع کنند .
               </CardBody>
             </Collapse>
           </Card>
@@ -66,7 +66,77 @@ class FAQ extends React.PureComponent {
                 ما درخواست فضای کار میکنید و ما به جای شما به همه فضاهای کاری
                 مشترک در شهر درخواستی درخواست میدهیم و پیشنهادشان را به شما
                 ارسال می کنیم و شما از بین پیشنهادهای رسیده بهترین را انتخاب
-                میکنید.
+                میکنید .
+              </CardBody>
+            </Collapse>
+          </Card>
+
+          <Card>
+            <CardHeader
+              id="question3"
+              onClick={this.toggleFaqCollapse}
+              className={
+                this.state.FAQCollapse === "question3" ? "activeHeader" : ""
+              }
+            >
+              <FontAwesomeIcon icon={faPlus} color="#6d8ae0" />
+              <FontAwesomeIcon icon={faMinus} color="#6d8ae0" />
+              آیا استفاده از استارتاپ اسپیس برای من هزینه دارد؟
+            </CardHeader>
+            <Collapse isOpen={this.state.FAQCollapse === "question3"}>
+              <CardBody>
+                خیر. تمامی مراحل درخواست فضای کار از زمان ارسال درخواست تا
+                دریافت پیشنهاد از فضاهای کار و تایید توسط شما از طریق این پلتفرم
+                بصورت رایگان انجام میگیرد و شما هزینه ای بابت خدماتی که ما ارایه
+                می کنیم به ما پرداخت نمی کنید .
+              </CardBody>
+            </Collapse>
+          </Card>
+
+          <Card>
+            <CardHeader
+              id="question4"
+              onClick={this.toggleFaqCollapse}
+              className={
+                this.state.FAQCollapse === "question4" ? "activeHeader" : ""
+              }
+            >
+              <FontAwesomeIcon icon={faPlus} color="#6d8ae0" />
+              <FontAwesomeIcon icon={faMinus} color="#6d8ae0" />
+              امنیت اطلاعات در استارتاپ اسپیس چگونه است؟
+            </CardHeader>
+            <Collapse isOpen={this.state.FAQCollapse === "question4"}>
+              <CardBody>
+                اطلاعات شما شامل اسم و مشخصات و شماره تلفن و تمامی اطلاعات شخصی
+                شما نزد ما نگهداری می شود و به هیچ عنوان برای مقاصد دیگر جز
+                پیشنهادهای فضای کار از طریق استارتاپ اسپیس استفاده نمی شود و
+                درخواستهای شما مستقیما برای ارائه دهندگان سرویس فضای کار ارسال
+                می شوید .
+              </CardBody>
+            </Collapse>
+          </Card>
+
+          <Card>
+            <CardHeader
+              id="question5"
+              onClick={this.toggleFaqCollapse}
+              className={
+                this.state.FAQCollapse === "question5" ? "activeHeader" : ""
+              }
+            >
+              <FontAwesomeIcon icon={faPlus} color="#6d8ae0" />
+              <FontAwesomeIcon icon={faMinus} color="#6d8ae0" />
+              طرز کار استارتاپ اسپیس چگونه است؟
+            </CardHeader>
+            <Collapse isOpen={this.state.FAQCollapse === "question5"}>
+              <CardBody>
+                شما یک فرم چند مرحله ای ساده را که به منظور شناسایی نیازهای
+                شماست پر کرده و ارسال می کنید و از داخل پنل خود بعد از ورود می
+                توانید درخواستهای خود را مشاهده و از آخرین وضعیت درخواست خود
+                مطلع می شوید و در صورت دریافت پیشنهاد می توانید قبول یا رد
+                نمایید. شما امکان انتخاب چند پیشنهاد را نیز دارید و در نهایت
+                فضای کاری مطلوب خود را انتخاب کرده و مراحل مستقر شدن و جابه جایی
+                را تا انتها به سرانجام میرسانید .
               </CardBody>
             </Collapse>
           </Card>
