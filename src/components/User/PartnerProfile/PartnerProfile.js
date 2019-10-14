e509f7f583b358dbb8406483079405d551808036
daniel <daniel.lalasa@gmail.com>
2019-10-14 13:38:34 +0330

    location direction property changed

--- src/components/User/PartnerProfile/PartnerProfile.js
+++ src/components/User/PartnerProfile/PartnerProfile.js
@@ -608,8 +608,9 @@ export default class PartnerProfile extends React.Component {
                 height="100%"
                 width="100%"
               /> */}
+
               <a
-                href={`https://www.google.com/maps/search/?api=1&query=${SafeValue(
+                href={`https://www.google.com/maps/dir/?api=1&query=${SafeValue(
                   location,
                   "latitude",
                   "string",
